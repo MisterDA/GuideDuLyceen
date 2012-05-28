7098650862910780d02a55c6c9c6b39843f4a748
MisterDA <antonin.decimo@gmail.com>
2012-05-28 16:20:22 +0200

    Encore un nouveau test résolu.

--- test.php
+++ test.php
@@ -1,7 +1,12 @@
 <?php
     session_start();
-    $a = 1;
-    echo $a;
+    try {
+		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+		$bdd = new PDO('mysql:host=sql.toile-libre.org;dbname=gdlc_oneofthem', 'gdlc_oneofthem', 'R2FZpw1F', $pdo_options);
+	}
+	catch (Exception $e) {
+		die('Erreur : ' . $e->getMessage());
+	}
 ?>
 
 <!DOCTYPE html>
@@ -13,11 +18,37 @@
 
 <body>
     <?php
-        echo $a;
-        if (1) {
-        	$b = 2;
-        }
-        echo $b;
+    
+    $a = 1;
+    
+	$printArticle = false;
+	if (!isset($_GET['article'])) { // Si $_GET['article'] n'est pas défini
+		$printLastArticle = true;
+		$a = 2;
+	}
+	if ($_GET['article'] == NULL) { // Si $_GET['article'] ne vaut rien
+		$printLastArticle = true;
+		$a = 3;
+	}
+	if (isset($_GET['article']) AND $_GET['article'] != NULL)  {
+		$a = 4;
+		$linksInBDD = $bdd->query('SELECT * FROM article'); // Recherche des liens dans la BDD
+		$a = 5;
+		while ($link = $linksInBDD->fetch) {
+			if ($link['link'] == $_GET['article']) { 
+				$printArticle = true;
+				$a = 6;
+			}
+			else {
+				$printLastArticle = true;
+				$a = 7;
+			}
+		}
+	}
+	
+	echo '$printLastArticle = |' . $printLastArticle . '|<br />$printArticle = |' . $printArticle . '|<br />';
+	echo $a;
+	
     ?>
 </body>
 
